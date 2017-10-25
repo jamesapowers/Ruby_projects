9a36cc009cb0117a7b64aabcd6954ca9bd9e7677
James Powers <powersajames@gmail.com>
2017-10-25 09:29:13 -0700

    completed game.rb

--- W2D2/chess/NullPiece.rb
+++ W2D2/chess/NullPiece.rb
@@ -1,12 +1,17 @@
+require 'singleton'
+require_relative 'piece'
 
 class NullPiece < Piece
+  attr_reader :symbol
   include Singleton
-  def initialize(position)
-    @piece = " "
-    @position = position
+
+  def initialize
+    @symbol = " "
+    @color = :none
   end
 
-  def moves()
+  def moves
+    []
   end
 
 end
--- W2D2/chess/piece.rb
+++ W2D2/chess/piece.rb
@@ -1,38 +1,49 @@
-class Piece
-  attr_reader :board, :color
-  attr_accessor :pos
+require_relative 'pieces/piece'
+require_relative 'pieces/bishop'
+require_relative 'pieces/king'
+require_relative 'pieces/knight'
+require_relative 'pieces/pawn'
+require_relative 'pieces/queen'
+require_relative 'pieces/rook'
+require_relative 'pieces/null'
+require_relative 'pieces/stepable'
+require_relative 'pieces/slideable'
 
-  def initialize(color, board, pos)
-    #raise 'invalid color' unless [:white, :black].include?(color)
-    #raise 'invalid pos' unless board.valid_pos?(pos)
-
-    @color, @board, @pos = color, board, pos
-
-    #board.add_piece(self, pos)
-  end
-
-  def to_s
-    " #{symbol} "
-  end
-
-  # def empty?
-  #   self.is_a?(NullPiece) ? true : false
-  # end
-  #
-  def symbol
-    # subclass implements this with unicode chess char
-    raise NotImplementedError
-  end
-  #
-  # def valid_moves
-  #   moves.reject { |end_pos| move_into_check?(end_pos) }
-  # end
-  #
-  # private
-  #
-  # def move_into_check?(end_pos)
-  #   test_board = board.dup
-  #   test_board.move_piece!(pos, end_pos)
-  #   test_board.in_check?(color)
-  # end
-end
+# class Piece
+#   attr_reader :board, :color
+#   attr_accessor :pos
+#
+#   def initialize(color, board, pos)
+#     #raise 'invalid color' unless [:white, :black].include?(color)
+#     #raise 'invalid pos' unless board.valid_pos?(pos)
+#
+#     @color, @board, @pos = color, board, pos
+#
+#     #board.add_piece(self, pos)
+#   end
+#
+#   def to_s
+#     " #{symbol} "
+#   end
+#
+#   # def empty?
+#   #   self.is_a?(NullPiece) ? true : false
+#   # end
+#   #
+#   def symbol
+#     # subclass implements this with unicode chess char
+#     raise NotImplementedError
+#   end
+#   #
+#   # def valid_moves
+#   #   moves.reject { |end_pos| move_into_check?(end_pos) }
+#   # end
+#   #
+#   # private
+#   #
+#   # def move_into_check?(end_pos)
+#   #   test_board = board.dup
+#   #   test_board.move_piece!(pos, end_pos)
+#   #   test_board.in_check?(color)
+#   # end
+# end
